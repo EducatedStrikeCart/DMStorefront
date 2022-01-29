48a33eb1a4e15840f2f70ffa9f5cf564bf7114b4
EducatedStrikeCart <dhn004@gmail.com>
2022-01-29 06:42:27 -0500

    File aditions for conversion to cookie based auth

---
+++ DMStorefront/Shared/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DMStorefront.Shared
+{
+    public class UserInfo
+    {
+        public bool IsAuthenticated { get; set; }
+        public string UserName { get; set; }
+        public Dictionary<string, string> ExposedClaims { get; set; }
+    }
+}
