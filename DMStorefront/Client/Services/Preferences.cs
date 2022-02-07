de8646510f53d57bc839c240ee26052cd7b224fa
EducatedStrikeCart <dhn004@gmail.com>
2022-02-07 17:46:40 -0500

    create profile service to store/read preferences from local storage

---
+++ DMStorefront/Client/Services/Preferences.cs
@@ -0,0 +1,8 @@
+﻿namespace DMStorefront.Client.Services
+{
+    public record Preferences
+    {
+        public bool UserMode { get; init; }
+    }
+
+}
---
+++ DMStorefront/Client/Services/ProfileService.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+
+namespace DMStorefront.Client.Services
+{
+    public class ProfileService
+    {
+        private readonly ILocalStorageService _localStorageService;
+        public event Action<Preferences> OnChange;
+
+        public ProfileService(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public async Task ToggleUserMode()
+        {
+            var preferences = await GetPreferences();
+            var newPreferences = preferences
+                with
+            { UserMode = !preferences.UserMode };
+            await _localStorageService.SetItemAsync("preferences", newPreferences);
+
+            OnChange?.Invoke(newPreferences);
+        }
+
+        public async Task<Preferences> GetPreferences()
+        {
+            return await _localStorageService.GetItemAsync<Preferences>("preferences")
+                ?? new Preferences();
+        }
+
+    }
+}
