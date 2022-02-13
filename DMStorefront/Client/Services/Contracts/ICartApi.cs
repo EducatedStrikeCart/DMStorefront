07a31a8867e287f776982e7ebf139a2f9f419f5f
EducatedStrikeCart <dhn004@gmail.com>
2022-02-13 02:41:56 -0500

    Update Cart API to use CartEntry object which tracks Item, Quantity, and Subtotal

--- DMStorefront/Client/Services/Contracts/ICartApi.cs
+++ DMStorefront/Client/Services/Contracts/ICartApi.cs
@@ -1,11 +1,12 @@
-﻿using DMStorefront.Shared.Models;
+﻿using DMStorefront.Client.Services.ServiceModels;
+using DMStorefront.Shared.Models;
 
 namespace DMStorefront.Client.Services.Contracts
 {
     public interface ICartApi
     {
-        Task Add(string item);
-        Task Remove(string item);
-        Task<Dictionary<string, int>> GetCart();
+        Task Add(Item item);
+        Task Remove(Item item);
+        Task<List<CartEntry>> GetCart();
     }
 }
---
+++ DMStorefront/Client/Services/ServiceModels/CartEntry.cs
@@ -0,0 +1,20 @@
+﻿using DMStorefront.Shared.Models;
+
+namespace DMStorefront.Client.Services.ServiceModels
+{
+    public class CartEntry
+    {
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public int PriceSubTotal
+        {
+            get { return Item.Price * Quantity; }
+        }
+
+        public CartEntry(Item item, int quantity)
+        {
+            Item = item;
+            Quantity = quantity;
+        }
+    }
+}
