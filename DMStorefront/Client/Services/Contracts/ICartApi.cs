43d13aa8c78d8996361a794c9fdb77539f82f4e9
EducatedStrikeCart <dhn004@gmail.com>
2022-02-12 22:42:37 -0500

    Implement and register Cart API

---
+++ DMStorefront/Client/Services/Contracts/ICartApi.cs
@@ -0,0 +1,12 @@
+﻿using DMStorefront.Shared.Models;
+
+namespace DMStorefront.Client.Services.Contracts
+{
+    public interface ICartApi
+    {
+        Task Add(string item);
+        Task UpdateQuantity(string item);
+        Task Remove(string item);
+        Task<List<Item>> GetItems(string itemString);
+    }
+}
---
+++ DMStorefront/Client/Services/Implementations/CartApi.cs
@@ -0,0 +1,72 @@
+﻿using Blazored.LocalStorage;
+using DMStorefront.Shared.Models;
+using System.Net.Http.Json;
+
+namespace DMStorefront.Client.Services.Implementations
+{
+    public class CartApi
+    {
+        public event Action? OnChange;
+        public Dictionary<string, int>? CartItems { get; set; }
+
+        private readonly ILocalStorageService _localStorageService;
+
+        public CartApi(ILocalStorageService localStorageService)
+        {
+            _localStorageService = localStorageService;
+        }
+
+        public async Task Add(string itemName)
+        {
+            Dictionary<string, int> newCart = await GetCart();
+            if (newCart.ContainsKey(itemName))
+            {
+                newCart[itemName] += 1;
+            }
+            else
+            {
+                newCart.Add(itemName, 1);
+            }
+            await _localStorageService.SetItemAsync("cart", newCart);
+
+            OnChange?.Invoke();
+        }
+
+        public async Task Remove(string itemName)
+        {
+            Dictionary<string, int> newCart = await GetCart();
+            if (newCart.ContainsKey(itemName))
+            {
+                if (newCart[itemName] == 1)
+                {
+                    newCart.Remove(itemName);
+                }
+                else
+                {
+                    newCart[itemName] -= 1;
+                }
+            }
+            await _localStorageService.SetItemAsync("cart", newCart);
+
+            OnChange?.Invoke();
+        }
+
+        public async Task<Dictionary<string, int>> GetCart()
+        {
+            Dictionary<string, int> cart = new();
+            cart = await _localStorageService.GetItemAsync<Dictionary<string, int>>("cart");
+            if (cart == null)
+
+            {
+                Dictionary<string, int> newCart = new();
+                await _localStorageService.SetItemAsync("cart", newCart);
+                return newCart;
+            }
+            else
+            {
+                return await _localStorageService.GetItemAsync<Dictionary<string, int>>("cart");
+            }
+
+        }
+    }
+}
