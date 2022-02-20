3db49ee234cdbfe6f92b5376e86256c142936ec3
EducatedStrikeCart <dhn004@gmail.com>
2022-02-20 09:22:10 -0500

    added profile service which holds user preference to see DM or Player side of the store

---
+++ DMStorefront/Client/Services/Contracts/IProfileService.cs
@@ -0,0 +1,11 @@
+﻿using DMStorefront.Client.Services.ServiceModels;
+
+namespace DMStorefront.Client.Services.Contracts
+{
+    public interface IProfileService
+    {
+        Task ToggleUserMode();
+
+        Task<Preferences> GetPreferences();
+    }
+}
