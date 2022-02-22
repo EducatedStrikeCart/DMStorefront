20f418969fd477ae3a2445a4769f575ce90e6b18
Tariq-Rasheed1212 <77362462+Tariq-Rasheed1212@users.noreply.github.com>
2022-02-21 19:12:58 -0500

    About page

---
+++ DMStorefront/Shared/PlayerCheckout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMStorefront.Shared
+
+    {     
+    public class PlayerCheckout
+            {
+            [Key]
+            public string ItemId { get; set; }
+
+            public string CartId { get; set; }
+
+            public int Quantity { get; set; }
+
+            public virtual PlayerCheckout Product { get; set; }
+
+            }
+    }
+    
